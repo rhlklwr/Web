bc7d25a5c5da65e1625cf619e84364f82e3ffe96
rhlklwr <kalwar.rahul@gmail.com>
2020-12-13 23:01:04 +0530

    added password reset

--- Flask/flask_blog/routes.py
+++ Flask/flask_blog/routes.py
@@ -1,18 +1,20 @@
 import secrets
 import os
 from flask import render_template, url_for, flash, redirect, request, abort
-from flask_blog import app, db, bcrypt
+from flask_blog import app, db, bcrypt, mail
 from PIL import Image
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_blog.forms import (RegistrationForm, LoginForm, UpdateAccountForm, 
+                              PostForm, RequestResetForm, ResetPasswordForm)
 from flask_blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=2)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
@@ -45,7 +47,7 @@ def login():
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash("Login unsuccessfull, Please check password or email", 'danger')
+            flash("Login unsuccessful, Please check password or email", 'danger')
     return render_template("login.html", title="Login", form=form)
 
 
@@ -131,3 +133,55 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template("user_posts.html", posts=posts, user=user)
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', 
+                  sender='noreply@demo.com', 
+                  recipients=[user.email])
+    msg.body = f"""To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+Above link will be valid for 30 minutes only.
+If you did not make this request, please ignore this email!!!
+"""
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("An email has been sent with instruction to reset password", "info")
+        return redirect(url_for('login'))
+    return render_template("reset_request.html", title='Reset Password', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is an invalid or expired token", "warning")
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash(f"Password changed!!!", "success")
+        return redirect(url_for("login"))
+    return render_template("reset_token.html", title='Reset Password', form=form)
+        
