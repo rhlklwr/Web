4f9adfefd4c615809c8692d3e4807293f23ac870
rhlklwr <kalwar.rahul@gmail.com>
2020-12-14 15:03:04 +0530

    almost complete

--- Flask/flask_blog/users/forms.py
+++ Flask/flask_blog/users/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-import email_validator
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flask_blog.models import User
-
+import email_validator
 
 class RegistrationForm(FlaskForm):
     
@@ -68,12 +67,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken already, please chosse diffrent one!')
             
-            
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-    
 class RequestResetForm(FlaskForm):
     email = StringField(
         'Email', validators=[DataRequired(), Email()],
