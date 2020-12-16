00061da840eaa7d20cc1d839f260516f40293fae
rhlklwr <kalwar.rahul@gmail.com>
2020-12-16 15:14:16 +0530

    WIP

--- Django/crm/account/urls.py
+++ Django/crm/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/', views.customers),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customers/<str:id>', views.customers, name='customers'),
 ]
