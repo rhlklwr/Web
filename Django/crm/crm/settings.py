46664008aaa907f29170b9a7a9adb13e82680e11
rhlklwr <kalwar.rahul@gmail.com>
2020-12-19 19:37:37 +0530

    Back to sqllite

--- Django/crm/crm/settings.py
+++ Django/crm/crm/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CRM',
-        'USER': 'postgres',
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
