a2975da15933a04f14bc6bf8c5631b86ab25d0c9
rhlklwr <kalwar.rahul@gmail.com>
2020-12-19 19:35:54 +0530

    Migrated to postgres But will rever back in next commit

--- Django/crm/account/urls.py
+++ Django/crm/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,4 +14,13 @@ urlpatterns = [
     path('create_order/<str:id>', views.createOrder, name='create_order'),
     path('update_order/<str:id>', views.updateOrder, name='update_order'),
     path('delete_order/<str:id>', views.deleteOrder, name='delete_order'),
+    
+    path('reset_password/', 
+         auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_done.html'), name='password_reset_complete'),
 ]
--- Django/crm/crm/settings.py
+++ Django/crm/crm/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRM',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -129,3 +133,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = Path(BASE_DIR).joinpath('static/images')
+
+# SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL')
+EMAIL_HOST_PASSWORD = config('PASSWORD')
