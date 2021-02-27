f984d7a4f68d6896e7727a5bc155ad17d2f84bca
rhlklwr <kalwar.rahul@gmail.com>
2021-02-27 22:15:17 +0530

    Background generator project

---
+++ Javascript/JAVASCRIPT/main.js
@@ -0,0 +1,24 @@
+// function New(say="New") {
+//     console.log(say);
+//     return say
+// }
+
+// New("Hello")
+
+// var a = ["Hello", 1, alert]
+
+// var user = {
+//     name : "John",
+//     "age" : 30
+// }
+
+// var list = [1,2,3,4,5,6,7,8,9,10]
+
+// // for (var i=0; i<list.length; i++){
+// //     console.log(list[i])
+// // }
+
+// list.forEach(function(i, idx){
+//     console.log(i)
+//     console.log(idx)
+// })
---
+++ VS code/test.py
@@ -0,0 +1,9 @@
+def yes():
+    hello = {
+        'a':5,
+        'b':6,
+    }
+    print(hello)
+
+print(yes())
+
