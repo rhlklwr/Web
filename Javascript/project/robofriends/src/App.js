11c35188c77b0728ec71563403d0ccca5d341209
rhlklwr <kalwar.rahul@gmail.com>
2021-03-01 23:02:12 +0530

    Now starting robo project

--- Javascript/project/robofriends/src/App.js
+++ Javascript/project/robofriends/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component {
+  render (){
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- Javascript/project/robofriends/src/index.js
+++ Javascript/project/robofriends/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'tachyons';
+// import App from './App';
+import Hello from './Hello'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <h1>Hello World</h1>,
+  <Hello greeting={'Hello react ninja'}/>,
   document.getElementById('root')
 );
 
