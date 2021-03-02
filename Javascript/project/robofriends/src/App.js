f8cd631555c691ce5513e41f43bb994b93de2fe9
rhlklwr <kalwar.rahul@gmail.com>
2021-03-02 17:25:08 +0530

    added scroll bar

--- Javascript/project/robofriends/src/App.js
+++ Javascript/project/robofriends/src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import './App.css'
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import './App.css';
+import Scroll from './Scroll';
 
 
 class App extends Component{
@@ -32,11 +33,13 @@ class App extends Component{
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                < CardList robots={filterRobots}/>
+                <Scroll>
+                    < CardList robots={filterRobots}/>
+                </Scroll>
             </div>
         );
     }
     
 }
 
-export default App
+export default App;
