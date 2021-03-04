75360a7a16b39bc77209dcedcc91e29f5f5d2982
rhlklwr <kalwar.rahul@gmail.com>
2021-03-04 11:49:38 +0530

    Frontend is completed

--- Javascript/project/smartbrain/src/App.js
+++ Javascript/project/smartbrain/src/App.js
@@ -9,6 +9,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 
 
@@ -33,7 +35,27 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
     }
+  }
+
+  calculateFaceLocation = (data) =>{
+    const calrifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: calrifaiFace.left_col * width,
+      topRow: calrifaiFace.top_row * height,
+      rightCol: width - (calrifaiFace.right_col * width),
+      bottomRow: height - (calrifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    this.setState({box: box})
   }
 
   onInputChange = (event) =>{
@@ -43,16 +65,20 @@ class App extends Component {
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input})
     app.models.predict(FACE_DETECT_MODEL, this.state.input)
-    .then(
-    function(response){
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    },
-    function(err){
-      console.log(err)
-    }
-    );
-    console.log()
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
   }
+
+  onRouteChange = (route) =>{
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    }else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+      this.setState({route: route});
+    }
+    
+
 
   render(){
     return (
@@ -60,11 +86,23 @@ class App extends Component {
         <Particles className='particles'
         params={particlesOptions}
         />
-       <Navigation />
-       <Logo />
-       <Rank />
-       <ImageLinkForm onInputChange={this.onInputChange}onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+       <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+       {this.state.route === 'home'
+              ? <div>
+                  <Logo />
+                  <Rank />
+                  <ImageLinkForm 
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                  />
+                    <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+                </div>
+              :(
+                this.state.route === 'signin'
+                ? <SignIn onRouteChange={this.onRouteChange}/>
+                : <Register onRouteChange={this.onRouteChange} />
+              )
+          }
       </div>
     );
   }; 
