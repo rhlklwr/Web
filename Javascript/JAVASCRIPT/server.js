19f7bb8837c78af1bf6ed20d199550869f2efeb3
rhlklwr <kalwar.rahul@gmail.com>
2021-03-05 09:37:55 +0530

    Node

---
+++ Javascript/JAVASCRIPT/server.js
@@ -0,0 +1,9 @@
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//     response.setHeader('content-Type', 'text/html');
+//     response.end('<h1>Hello</h1>')
+// });
+
+// server.listen(3000);
+
