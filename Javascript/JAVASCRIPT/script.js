29bb9e0cc8f70d50a18dd77ef9383abcf3942759
rhlklwr <kalwar.rahul@gmail.com>
2021-03-05 11:53:12 +0530

    node FS

--- Javascript/JAVASCRIPT/script.js
+++ Javascript/JAVASCRIPT/script.js
@@ -1,4 +1,30 @@
-const a=5;
-const b=6;
+const fs = require('fs');
 
-console.log(a +b)
+fs.readFile('./temp.txt', (err, data) => {
+    if(err){
+        console.log(err);
+    }else{
+    console.log('Aync', data.toString('utf-8'));
+}
+})
+
+const file = fs.readFileSync('./temp.txt');
+console.log('Sync', file.toString())
+
+// fs.appendFile('./temp.txt', ' This is append!', err =>{
+//     if(err){
+//         console.log(err)
+//     }
+// })
+
+// fs.writeFile('./temp1.txt', 'Now I am writing!', err=>{
+//     if(err){
+//         console.log(err);
+//     }
+// })
+
+// fs.unlink('./temp1.txt', err=>{
+//     if(err){
+//         console.log(err);
+//     }
+// })
--- Javascript/JAVASCRIPT/server.js
+++ Javascript/JAVASCRIPT/server.js
@@ -7,3 +7,12 @@
 
 // server.listen(3000);
 
+const express  = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+app.listen(3000);
