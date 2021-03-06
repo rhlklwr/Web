86d39e9d5dd9e6b9beb258347f8beb074b0067c4
rhlklwr <kalwar.rahul@gmail.com>
2021-03-06 17:58:52 +0530

    Checkpoint1 while connecting backend and frontend

--- Javascript/project/smartbrain/src/App.js
+++ Javascript/project/smartbrain/src/App.js
@@ -38,7 +38,25 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+         id: '',
+         name: '',
+         email: '',
+         entries: 0,
+         joined: '',
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user:{
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
+    }
+    })
   }
 
   calculateFaceLocation = (data) =>{
@@ -90,7 +108,7 @@ class App extends Component {
        {this.state.route === 'home'
               ? <div>
                   <Logo />
-                  <Rank />
+                  <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                   <ImageLinkForm 
                   onInputChange={this.onInputChange}
                   onButtonSubmit={this.onButtonSubmit}
@@ -99,8 +117,8 @@ class App extends Component {
                 </div>
               :(
                 this.state.route === 'signin'
-                ? <SignIn onRouteChange={this.onRouteChange}/>
-                : <Register onRouteChange={this.onRouteChange} />
+                ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               )
           }
       </div>
