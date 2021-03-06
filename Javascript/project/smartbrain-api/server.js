8bee7ef5a3e852eb48ce1102327f2218af3dcc98
rhlklwr <kalwar.rahul@gmail.com>
2021-03-06 14:39:46 +0530

    layout for api is created

--- Javascript/project/smartbrain-api/server.js
+++ Javascript/project/smartbrain-api/server.js
@@ -1,11 +1,84 @@
+
 const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
+const database = {
+    users: [
+        {id: '123',
+         name: 'Rahul',
+         email: 'temp@temp.com',
+         password: 'password',
+         entries: 0,
+         joined: new Date(),
+    },
+    {id: '124',
+         name: 'Rahul1',
+         email: 'temp1@temp.com',
+         password: 'password',
+         entries: 0,
+         joined: new Date(),
+    }
+    ]
+}
+
 app.get('/', (req, res) => {
-    res.send("This is working")
+    res.send(database.users);
 })
 
+app.post('/signin', (req, res) =>{
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password){
+            res.json("success");
+        }else{
+            res.status(400).json("error login in")
+        }
+    res.json('signin')
+})
+
+app.post('/register', (req,res) =>{
+    const {email, name, password} = req.body;
+    database.users.push(
+        {id: '125',
+         name: name,
+         email: email,
+         password: password,
+         entries: 0,
+         joined: new Date(),
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) =>{
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach( user => {
+        if(user.id === id){
+            found === true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json("Not found")
+    }
+})
+
+app.post('/image', (req, res) =>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach( user => {
+        if(user.id === id){
+            found === true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json("Not found")
+    }
+})
 app.listen(3000, () =>{
     console.log("App is running");
 });
