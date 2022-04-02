637493271c48a11b50a2ca41d9e2d086957bf280
rhlklwr <kalwar.rahul@gmail.com>
2022-04-02 12:31:23 +0530

    react query

--- react-query-tut/src/components/Planets.js
+++ react-query-tut/src/components/Planets.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from 'react-query';
 
 
 import Planet from "./Planet";
-const fetchPlanets = async () => {
-    const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key) => {
+    const res = await fetch(`http://swapi.dev/api/planets/?page=${key.queryKey[1]}`);
     return res.json();
 }
 
 const Planets = () => {
-    const { data, status } = useQuery('planets', fetchPlanets);
+    const [page, setPage] = useState(1);
+    const { data, status } = useQuery(['planets', page], fetchPlanets, {
+        staleTime: 2000,
+    });
 
     return ( 
         <div>
             <h2>Planets</h2>
+            <button onClick={() => setPage(1)}>Page 1</button>
+            <button onClick={() => setPage(2)}>Page 2</button>
+            <button onClick={() => setPage(3)}>Page 3</button>
             {status  === 'loading' && (
                 <div>Loading data...</div>
             )}
